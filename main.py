76dc2eee1eaf65f9df5b119c10e387c8f52fe936
Shreyashi-chowdhury <162280048+Shreyashi-chowdhury@users.noreply.github.com>
2024-03-05 11:37:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+# #heterogram
+# s= input()
+# s_set = []
+# flag = 1
+# for i in s:
+#     if i in s_set:
+#         flag = 0
+#         break
+#     else: 
+#         s_set.append(i)
+# if(flag==1):
+#     print("Heterogram")
+# else:
+#     print("not heterogram")
+
+
+
+# # n = 5
+# # a = 1
+# # b = 0
+
+# # for i in range(n):
+# #     for j in range(n):
+# #         if(j%2 == 0):
+# #             print(a,end='')
+# #         else:
+# #             print(b,end = '')
+# #     print()
+# #     c = b
+# #     b = a
+# #     a = c
+
+
+# '''
+# 10101
+# 01010
+# 10101
+# 01010
+# 10101
+# '''
+
+l1 = ['a','aba','abcba','ab']
+
+for i in range(len(l1)):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
